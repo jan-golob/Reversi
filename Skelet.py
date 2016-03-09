7a91e2c9a90ccea828cbd211768e4d192afef40d
jan-golob <jangolob.jg@gmail.com>
2016-03-09 13:35:47 +0100

    Implementiraj algoritem MinMax, close #14,

--- Skelet.py
+++ Skelet.py
@@ -1,3 +1,6 @@
+import logging
+import threading
+
 class Deska:
     def __init__(self, igralec0="B", igralec1="W"):
        osnova =[[None]*8 for _ in range(8)]
@@ -120,108 +123,112 @@ def mprint (m):
 # print("")
 # b.izrisi()
 
+def nasprotnik(i):
+    if i == 0:
+        return 1
+    elif i == 1:
+        return 0
+    else:
+        assert False, "to narobe uporabljaš"
+
 
 class MinMax:
+    ZMAGA = 10000
+    INFI = ZMAGA + 109
     def __init__(self, globina):
-        def __init__(self, globina):
-            self.globina = globina  # do katere globine iščemo?
-            self.prekinitev = False # ali moramo končati?
-            self.pozicija = None # objekt, ki opisuje igro (ga dobimo kasneje)
-            self.jaz = None  # katerega igralca igramo (podatek dobimo kasneje)
-            self.poteza = None # sem napišemo potezo, ko jo najdemo
-
+        self.globina = globina  # do katere globine iščemo?
+        self.prekinitev = False # ali moramo končati?
+        self.pozicija = None # objekt, ki opisuje igro (ga dobimo kasneje)
+        self.jaz = None  # katerega igralca igramo (podatek dobimo kasneje)
+        self.poteza = None # sem napišemo potezo, ko jo najdemo
+        
     # samoumevno
     def prekini(self):
         """Metoda, ki jo pokliče GUI, če je treba nehati razmišljati, ker
            je uporabnik zaprl okno ali izbral novo igro."""
         self.prekinitev = True
 
+    # Zažene minmax iz njim upravlja
+    def optimalna_poteza(self, pozicija, igralec):
+        self.pozicija = pozicija
+        self.prekinitev = False # Glavno vlakno bo to nastvilo na True, če moramo nehati
+        self.jaz = igralec
+        self.poteza = None # Sem napišemo potezo, ko jo najdemo
+        # Poženemo minimax
+        (poteza, vrednost) = self.minimax(self.globina, True)
+        self.jaz = None
+        self.pozicija = None
+        if not self.prekinitev:
+            # Potezo izvedemo v primeru, da nismo bili prekinjeni
+            logging.debug("minimax: poteza {0}, vrednost {1}".format(poteza, vrednost))
+            self.poteza = poteza
+    #
+    def minimax(self, globina, maximiziramo):
+        if self.prekinitev:
+            # Sporočili so nam, da moramo prekiniti
+            logging.debug ("Minimax prekinja, globina = {0}".format(globina))
+            return (None, 0)
+        #konec igre
+        (konec, razmerje) = self.pozicija.alije_konec()
+        if konec:
+            (i0,i1) = razmerje
+            if self.jaz == 0 and i0 > i1:
+                return (None, MinMax.ZMAGA)
+            elif self.jaz == 1 and i1 > i0:
+                return (None, MinMax.ZMAGA)
+            elif self.jaz == 0 and i1 > i0:
+                return (None, -MinMax.ZMAGA)
+            elif self.jaz == 1 and i0 > i1:
+                return (None, -MinMax.ZMAGA)
+            else:
+                assert False, "neki je narobe a je mogoče neodločeno ali pa jst ne znam programirat"
+        else:
+            if globina == 0:
+                return (None, self.hevristika(self.jaz))
+            else:
+                # Naredimo eno stopnjo minimax
+                kopija = self.pozicija.copy()
+                if maximiziramo:
+                    # Maksimiziramo
+                    najboljsa_poteza = None
+                    vrednost_najboljse = -MinMax.INFI
+                    for p in self.pozicija.moznosti(self.jaz):
+                        self.pozicija.postavi(self.jaz, p)
+                        vrednost = self.minimax(globina-1, not maximiziramo)[1]
+                        self.pozicija = kopija
+                        if vrednost > vrednost_najboljse:
+                            vrednost_najboljse = vrednost
+                            najboljsa_poteza = p
+                else:
+                    # Minimiziramo
+                    najboljsa_poteza = None
+                    vrednost_najboljse = MinMax.INFI
+                    for p in self.pozicija.moznosti(nasprotnik(self.jaz)):
+                        self.pozicija.postavi(nasprotnik(self.jaz), p)
+                        vrednost = self.minimax(globina-1, not maximiziramo)[1]
+                        self.pozicija = kopija
+                        if vrednost < vrednost_najboljse:
+                            vrednost_najboljse = vrednost
+                            najboljsa_poteza = p
+                assert (najboljsa_poteza is not None), "minimax: izračunana poteza je None"
+                return (najboljsa_poteza, vrednost_najboljse)
+                
+        
     # izračuna vrednost pozicije, zankrat le izpiše koliko so vredna posamezna polja
-    def hevristika(self,pozicija,igralec):
-        vrednosti = {
-            #koti
-            (0,0) : 100,
-            (0,7) : 100,
-            (7,0) : 100,
-            (7,7) : 100,
-            #ob robovih zraven kota
-            (0,1) : -20,
-            (0,6) : -20,
-            (1,0) : -20,
-            (1,7) : -20,
-            (6,0) : -20,
-            (6,7) : -20,
-            (7,1) : -20,
-            (7,6) : -20,
-            #diagonalno na kot = DeadZONE
-            (1,1) : -50,
-            (1,6) : -50,
-            (6,1) : -50,
-            (6,6) : -50,
-            #na robu ob deadzone
-            (0,2) : 10,
-            (0,5) : 10,
-            (2,0) : 10,
-            (2,7) : 10,
-            (5,0) : 10,
-            (5,7) : 10,
-            (7,2) : 10,
-            (7,5) : 10,
-            #na sredini roba
-            (0,3) : 5,
-            (0,4) : 5,
-            (3,0) : 5,
-            (4,0) : 5,
-            (3,7) : 5,
-            (4,7) : 5,
-            (7,3) : 5,
-            (7,4) : 5,
-            #ob robu
-            (1,2) : -2,
-            (1,3) : -2,
-            (1,4) : -2,
-            (1,5) : -2,
-            #
-            (2,1) : -2,
-            (3,1) : -2,
-            (4,1) : -2,
-            (5,1) : -2,
-            #
-            (2,6) : -2,
-            (3,6) : -2,
-            (4,6) : -2,
-            (5,6) : -2,
-            #
-            (6,2) : -2,
-            (6,3) : -2,
-            (6,4) : -2,
-            (6,5) : -2,
-            #sredina
-            (2,2) : -1,
-            (2,3) : -1,
-            (2,4) : -1,
-            (2,5) : -1,
-
-            (3,2) : -1,
-            (3,3) : -1,
-            (3,4) : -1,
-            (3,5) : -1,
-
-            (4,2) : -1,
-            (4,3) : -1,
-            (4,4) : -1,
-            (4,5) : -1,
-
-            (5,2) : -1,
-            (5,3) : -1,
-            (5,4) : -1,
-            (5,5) : -1,
-        }
-        test = Deska()
+    def hevristika(self,igralec):
+        vrednosti= [[100, -20, 10, 5, 5, 10, -20, 100], [-20, -50, -2, -2, -2, -2, -50, -20],[10, -2, -1, -1, -1, -1, -2, 10], [5, -2, -1, -1, -1, -1, -2, 5],
+            [5, -2, -1, -1, -1, -1, -2, 5], [10, -2, -1, -1, -1, -1, -2, 10], [-20, -50, -2, -2, -2, -2, -50, -20], [100, -20, 10, 5, 5, 10, -20, 100]]
+        ocena = 0
         for x in range(8):
             for y in range(8):
-                test.ploskev[x][y] = vrednosti[(x,y)]
-        test.izrisi()
+                if self.pozicija.ploskev[x][y] == self.pozicija.protagonist(igralec):
+                    ocena += vrednosti[x][y]
+                elif self.pozicija.ploskev[x][y] == self.pozicija.antagonist(igralec):
+                    ocena -= vrednosti[x][y]
+        return ocena
+        
 
-tata = MinMax(4)
-tata.hevristika((2),1)
+
+tata = MinMax(2)
+print(tata.optimalna_poteza(Deska(),0))
+print(tata.poteza)
